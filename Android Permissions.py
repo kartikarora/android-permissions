35f272ab9627edcef1d60424fe442b8f15ce4288
Kartik Arora <chipset95@yahoo.co.in>
2017-01-22 16:01:17 +0530

    Detail ready

--- Android Permissions.py
+++ Android Permissions.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask, render_template, json
+from flask import Flask, render_template, json, jsonify
+from flask import make_response
 
 app = Flask(__name__)
 
@@ -23,12 +24,26 @@ def list():
     return render_template('layout.html', container='list', perms_names=perms_names)
 
 
+@app.route('/list/json', methods=['GET', 'POST'])
+def list_api():
+    return make_response(jsonify(perms_names), 200)
+
+
 @app.route('/detail/<string:permission>')
 def detail(permission):
     name = permission
     permission = perms_info[name]
     permission['name'] = name
     return render_template('layout.html', container='detail', permission=permission)
+
+
+@app.route('/detail/<string:permission>/json', methods=['GET', 'POST'])
+def detail_api(permission):
+    name = permission
+    permission = perms_info[name]
+    permission['name'] = name
+    permission['url'] = 'https://developer.android.com/reference/android/Manifest.permission.html#' + name
+    return make_response(jsonify(permission), 200)
 
 
 if __name__ == '__main__':
